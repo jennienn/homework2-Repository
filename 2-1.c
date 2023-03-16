fb37c752957ca9bfff53e397328680198bb243f5
jennienn <dpwb12@naver.com>
2023-03-16 16:55:00 +0900

    homework2 Repository

--- 2-1.c
+++ 2-1.c
@@ -8,24 +8,24 @@ int main()
 
 printf("----- [김예진] [2022041035] -----\n");
 
-printf("Size of char: %ld byte\n",sizeof(charType)); 
-printf("Size of int: %ld bytes\n",sizeof(integerType)); 
-printf("Size of float: %ld bytes\n",sizeof(floatType)); 
-printf("Size of double: %ld bytes\n",sizeof(doubleType));
+printf("Size of char: %ld byte\n",sizeof(charType)); //문자형 변수 charType의 byte 크기가 출력됨
+printf("Size of int: %ld bytes\n",sizeof(integerType)); //정수형 변수 integerType의 byte 크기가 출력됨
+printf("Size of float: %ld bytes\n",sizeof(floatType)); //실수형 변수 floatType의 byte 크기가 출력됨
+printf("Size of double: %ld bytes\n",sizeof(doubleType)); //실수형 변수 floaType의 byte 크기가 출력됨
 
 printf("-----------------------------------------\n");
 
-printf("Size of char: %ld byte\n",sizeof(char)); 
-printf("Size of int: %ld bytes\n",sizeof(int)); 
-printf("Size of float: %ld bytes\n",sizeof(float)); 
-printf("Size of double: %ld bytes\n",sizeof(double));
+printf("Size of char: %ld byte\n",sizeof(char)); //문자형 자료형 char의 byte 크기가 출력됨
+printf("Size of int: %ld bytes\n",sizeof(int)); //정수형 자료형 int의 byte 크기가 출력됨
+printf("Size of float: %ld bytes\n",sizeof(float)); //실수형 자료형 float의 byte 크기가 출력됨
+printf("Size of double: %ld bytes\n",sizeof(double)); //실수형 자료형 double의 byte 크기가 출력됨
 
 printf("-----------------------------------------\n");
 
-printf("Size of char*: %ld byte\n",sizeof(char*)); 
-printf("Size of int*: %ld bytes\n",sizeof(int*)); 
-printf("Size of float*: %ld bytes\n",sizeof(float*)); 
-printf("Size of double*: %ld bytes\n",sizeof(double*));
+printf("Size of char*: %ld byte\n",sizeof(char*)); //문자형 포인터 char*의 byte 크기가 출력됨
+printf("Size of int*: %ld bytes\n",sizeof(int*)); //정수형 포인터 int*의 byte 크기가 출력됨
+printf("Size of float*: %ld bytes\n",sizeof(float*)); //실수형 포인터 float*의 byte 크기가 출력됨
+printf("Size of double*: %ld bytes\n",sizeof(double*)); //실수형 포인터 double*의 byte 크기가 출력됨
 
 return 0; 
 }
--- 2-2.c
+++ 2-2.c
@@ -11,46 +11,46 @@ i = 1234;
 printf("----- [김예진] [2022041035] -----\n");
 
 printf("[checking values before ptr = &i] \n"); 
-printf("value of i == %d\n", i); 
-printf("address of i == %p\n", &i); 
-printf("value of ptr == %p\n", ptr); 
-printf("address of ptr == %p\n", &ptr);
+printf("value of i == %d\n", i); // 변수i의 값이 출력됨
+printf("address of i == %p\n", &i); //변수 i의 주소값이 출력됨
+printf("value of ptr == %p\n", ptr); //포인터 ptr의 값이 할당되어 있지 않아 쓰레기 값이 출력됨
+printf("address of ptr == %p\n", &ptr); //포인터 ptr의 주소값이 출력됨
 
 ptr = &i;     /* ptr is now holding the address of i */
 
 printf("\n[checking values after ptr = &i] \n"); 
-printf("value of i == %d\n", i); 
-printf("address of i == %p\n", &i); 
-printf("value of ptr == %p\n", ptr); 
-printf("address of ptr == %p\n", &ptr); 
-printf("value of *ptr == %d\n", *ptr);
+printf("value of i == %d\n", i); //변수 i의 값이 출력됨
+printf("address of i == %p\n", &i); //변수 i의 주소값이 출력됨
+printf("value of ptr == %p\n", ptr); //변수 i의 주소값이 출력됨
+printf("address of ptr == %p\n", &ptr); //포인터 ptr의 주소값이 출력됨
+printf("value of *ptr == %d\n", *ptr); //변수 i의 값이 출력됨
 
 dptr = &ptr;   /* dptr is now holding the address of ptr */
 
 printf("\n[checking values after dptr = &ptr] \n"); 
-printf("value of i == %d\n", i);
-printf("address of i == %p\n", &i);
-printf("value of ptr == %p\n", ptr); 
-printf("address of ptr == %p\n", &ptr); 
-printf("value of *ptr == %d\n", *ptr); 
-printf("value of dptr == %p\n", dptr); 
-printf("address of dptr == %p\n", &dptr); 
-printf("value of *dptr == %p\n", *dptr);
-printf("value of **dptr == %d\n", **dptr);
+printf("value of i == %d\n", i); //변수 i의 값이 출력됨
+printf("address of i == %p\n", &i); //변수 i의 주소값이 출력됨
+printf("value of ptr == %p\n", ptr); //변수 i의 주소값이 출력됨
+printf("address of ptr == %p\n", &ptr); //포인터 ptr의 주소값이 출력됨
+printf("value of *ptr == %d\n", *ptr); //변수 i의 값이 출력됨
+printf("value of dptr == %p\n", dptr); //포인터 ptr의 주소값이 출력됨
+printf("address of dptr == %p\n", &dptr); //이중포인터 dptr의 주소값이 출력됨
+printf("value of *dptr == %p\n", *dptr); //변수 i의 주소값이 출력됨
+printf("value of **dptr == %d\n", **dptr); //변수 i의 값이 출력됨
 
 *ptr = 7777;    /* changing the value of *ptr */
 
-printf("\n[after *ptr = 7777] \n");
-printf("value of i == %d\n", i); 
-printf("value of *ptr == %d\n", *ptr);
-printf("value of **dptr == %d\n", **dptr);
+printf("\n[after *ptr = 7777] \n"); //포인터 ptr이 가르키고 있는 변수 i의 주소에 7777이 할당됨
+printf("value of i == %d\n", i); //변수 i의 값 출력됨
+printf("value of *ptr == %d\n", *ptr);//변수 i의 값 출력됨
+printf("value of **dptr == %d\n", **dptr);//변수 i의 값 출력됨
 
 **dptr = 8888;   /* changing the value of **dptr */
 
-printf("\n[after **dptr = 8888] \n");
-printf("value of i == %d\n", i); 
-printf("value of *ptr == %d\n", *ptr); 
-printf("value of **dptr == %d\n", **dptr);
+printf("\n[after **dptr = 8888] \n");//이중포인터 dptr이 최종적으로 가르키고 있는 변수 i의 주소에 8888이 할당됨
+printf("value of i == %d\n", i); //변수 i의 값 출력됨
+printf("value of *ptr == %d\n", *ptr); //변수 i의 값 출력됨
+printf("value of **dptr == %d\n", **dptr); //변수 i의 값 출력됨
 
 return 0;
 }
